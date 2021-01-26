2cbf1fe842868145aadca233953f1ef3b59fcd52
see24 <sarahendicott4@gmail.com>
2021-01-26 17:10:27 -0500

    get example ready for David

--- notReady/churchillExample.R
+++ notReady/churchillExample.R
@@ -3,11 +3,26 @@ library(dplyr)
 library(sf)
 devtools::load_all(".")
 
+# also need installed raster and stars packages
+
+# Download the ChurchillData.zip and ChurchillRoadsHarv.zip from here:
+# https://drive.google.com/drive/u/0/folders/155ZceLOw_-519NCHNOYQgeiQEIe_Qqca
+
+# path to folder where you unzipped the Churchill data
 data_path <- "../ChurchillAnalysis/inputNV/ChurchillData/"
 
+# Note this road data was separated into 2010 and 2020 based on Year constructed
+# in the MNRF data which is incomplete and not very accurate. Timing is VERY
+# APPROXIMATE
 roads <- st_read(paste0(data_path, "road_ORNMNRFCH2010.shp"))
 
+roads2020 <- st_read(paste0(data_path, "road_ORNMNRFCH2020.shp"))
+
+# provincial land cover data
 plc <- raster::raster(paste0(data_path, "plc50.tif"))
+
+# harvest polygons with year of harvest = yrdep
+harvCH <- st_read(paste0(data_path, "ChurchillRoadsHarv/harvPCIFRI.shp"))
 
 plc <- raster::aggregate(plc, fact = 5, fun = raster::modal)
 # make cost from plc
@@ -15,18 +30,18 @@ plc <- raster::aggregate(plc, fact = 5, fun = raster::modal)
 # TODO: These are made up costs, will need to define real ones with reasons.
 plcToCost <- tribble(
   ~plcClass, ~cost,
-        1:2, 0.9, # water
-          3, 0, # infrastructure/settlement,
-          4, 1, #  quarry/mine tailings
-          5, 0.25, # bedrock
-          6, 0.8, # mudflats
-        7:8, 0.25, # cuts, burns
-          9, 0.6, # regen depletion
-         10, 0.3, # sparse forest
-      11:13, 0.5, # dense forest
-      15:23, 0.8, # marsh, swamp, fen, bog
-      24:27, 0.25, # tundra, agriculture
-      28:29, 0.5 # other, assume forest
+  1:2, 0.9, # water
+  3, 0, # infrastructure/settlement,
+  4, 1, #  quarry/mine tailings
+  5, 0.25, # bedrock
+  6, 0.8, # mudflats
+  7:8, 0.25, # cuts, burns
+  9, 0.6, # regen depletion
+  10, 0.3, # sparse forest
+  11:13, 0.5, # dense forest
+  15:23, 0.8, # marsh, swamp, fen, bog
+  24:27, 0.25, # tundra, agriculture
+  28:29, 0.5 # other, assume forest
 ) %>% tidyr::unnest(cols = c(plcClass)) %>% as.matrix()
 
 cost <- raster::reclassify(plc, plcToCost)
@@ -38,48 +53,39 @@ cost <- raster::reclassify(plc, plcToCost)
 # use stars package instead
 cost_st <- stars::st_as_stars(cost)
 
+# rasterize roads to template
 tmplt <- stars::st_as_stars(st_bbox(cost_st), nx = raster::ncol(cost), 
                             ny = raster::nrow(cost), values = 1)
 
 roads_st <- stars::st_rasterize(roads %>% select(-YEAR_CO), template = tmplt, 
                                 options = "ALL_TOUCHED=TRUE") == 1
 
+# road raster is 1 where there are no roads and 0 where there are roads
 cost_st <- cost_st * roads_st
 
 # convert back to Raster from stars
 cost_st <- as(cost_st, "Raster")
 
-# make landings based on fri
-st_layers("../ChurchillAnalysis/inputNV/FRI/FRI_PCI_2020/fripci2020.gdb")
-
-# trout lake = 120, Caribou = 175, Lac Seul 702
-friTL <- st_read("../ChurchillAnalysis/inputNV/FRI/FRI_PCI_2020/fripci2020.gdb", "f120")
-friCar <- st_read("../ChurchillAnalysis/inputNV/FRI/FRI_PCI_2020/fripci2020.gdb", "f175")
-friLS <- st_read("../ChurchillAnalysis/inputNV/FRI/FRI_PCI_2020/fripci2020.gdb", "f702")
-
-friCH <- rbind(friTL, friCar) %>% rbind(friLS)
-
-harvCH <- filter(friCH, deptype == "HARVEST") %>% select(objid, yrdep)
-
-harvCH2 <- harvCH %>% group_by(yrdep) %>% 
-  summarise(objid = first(objid))
-
 # pick landings as points on the surface
-harvCH2010 <- harvCH2 %>% filter(yrdep == 2010) %>% st_cast("POLYGON") %>% 
+harvCH2010 <- harvCH %>% filter(yrdep == 2010) %>% 
   st_point_on_surface()
 
-rm(plc, friCH, friLS, friTL, friCar, harvCH, cost, roads_st, tmplt, harvCH2)
-
-harvCH2010 <-  harvCH2010 %>% st_transform(st_crs(cost_st))
+# transform to the same crs
 roads <- roads %>%  st_transform(st_crs(cost_st))
+harvCH2010 <- harvCH2010 %>%  st_transform(st_crs(cost_st))
 
+# run projection of roads to connect landings to existing road network
 roadsProjCH2010 <- projectRoadsNew(landings = harvCH2010, 
                                    cost = cost_st, 
                                    roads = roads)
 
-# 1,2 ,4 don't work but 3 does, what is the difference?
+# Look at maps in interactive mode, change to "plot" for non-interactive
 tmap::tmap_mode("view")
 
-tmap::qtm(cost_st)+
-  tmap::qtm(roads)+
-  tmap::qtm(harvCH2010 %>% slice(c(88, 89, 272)) %>% mutate(ID = 1:n()), col = "ID")
+tmap::qtm(roadsProjCH2010$costSurface)+
+  tmap::qtm(roadsProjCH2010$landings, dots.col = "red")+
+  tmap::qtm(roadsProjCH2010$roads, lines.col = "red")+
+  tmap::qtm(roads)
+# you might want to add these layers to compare to the polygons or 2020 roads
+  #tmap::qtm(harvCH2 %>% filter(yrdep == 2010))+
+  #tmap::qtm(roads2020)
