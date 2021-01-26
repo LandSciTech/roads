b1de9d94ccf6dd0305adf77552393f67c03248f4
see24 <sarahendicott4@gmail.com>
2021-01-26 15:49:24 -0500

    manage new roads that connect to old at both ends

--- R/pathsToLines.R
+++ R/pathsToLines.R
@@ -24,19 +24,18 @@ pathsToLines <- function(sim){
     sim$paths.v <<- sim$paths.v[-(inds[1]:inds[2]), ]
     
     ## index of where new road connects to existing road
-    conn <- match(1, er[v]) 
+    conn <- which(er[v] == 1)
     
-    if (!is.na(conn)){
-      ## remove portions that run along existing road, if applicable
-      if (conn==1){ 
-        ## if road starts with connection to existing road, reverse it and find
-        ## connection
-        v <- rev(v)
-        conn <- match(1,er[v])
+    ## remove portions that run along existing road, if applicable
+    if(length(conn) > 0){
+      keep <- which(er[v] == 0)
+      if(min(keep) > 1){
+        keep <- c(min(keep)-1, keep)
       }
-      
-      ## remove portions that run along existing road
-      v <- v[1:conn] 
+      if(max(keep) < length(v)){
+        keep <- c(keep, max(keep)+1)
+      }
+      v <- v[keep]
     }
     
     #id <- names(sim$paths.list)[[i]]
