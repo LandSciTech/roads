43ce969fde70f7cbe2ea87ade5123abc91c3b7b7
Josie Hughes <josie.hughes@canada.ca>
2021-02-17 21:45:14 +0000

    syncing.

--- R/getLandingsFromTarget.R
+++ R/getLandingsFromTarget.R
@@ -4,12 +4,12 @@ getLandingsFromTarget<-function(inputPatches,
                                 omitCentroidsOutOfPolygons=F){
   # Function to select a specific number of landings withing patches. Landing set
   # will include centroids, and additional randomly selected sample points if
-  # numLandings>numCentroids. 
-  
+  # numLandings>numCentroids.
+
   # inputPatches=newBlocks[[cm]]
-  
+
   inputPatches[inputPatches==0]=NA
-  
+
   landPts = matrix(0,0,3)
   colnames(landPts)=c("x","y","layer")
   for(i in 1:length(numLandings)){
@@ -21,34 +21,35 @@ getLandingsFromTarget<-function(inputPatches,
       landPts= rbind(landPts,raster::rasterToPoints(ip,fun=function(landings){landings>0}))
       next
     }
-    
+
     landC = getCentroids(ip,withIDs=T) #note centroids are not always in polygons
     if (omitCentroidsOutOfPolygons){
       landC[is.na(ip)]=NA
     }
     remL = ip;remL[landC>0]=NA
     numSamples = nl-raster::cellStats(landC>0,"sum")#select additional points so total number is equal to small alternative
-    
+
     landC = raster::rasterToPoints(landC,fun=function(landings){landings>0})
     #split into smaller patches to ensure adequate road density
     #sampleProp = 1/100
     #numSamples = round(cellStats(anthDist,"sum")*sampleProp)
-    
+
     landPts = rbind(landPts,landC)
-    
+
     if(numSamples<=0){
       next
     }
-    
+
     landingPts = raster::sampleStratified(remL, size=numSamples,xy=T)
     landingPts=landingPts[,2:4]
     #add centroids to ensure all patches are included
     landPts = rbind(landPts,landingPts)
-    
+
   }
-  
+
   # make sf object
-  landPts <- as.data.frame(landPts) %>% sf::st_as_sf(coords = c("x", "y"))
-  
+  landPts <- as.data.frame(landPts) %>%
+    sf::st_as_sf(coords = c("x", "y"), crs = sf::st_crs(inputPatches))
+
   return(landPts)
 }
