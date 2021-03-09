7aaeaa3ead734ef3a0e5ff98cb47155fd83e71be
see24 <sarahendicott4@gmail.com>
2021-03-09 11:47:51 -0500

    readd this script

---
+++ scripts/recreateExistingNetworkExample.R
@@ -0,0 +1,18 @@
+library(raster)
+#I think I should be able to recreate existing road network from a point on it, setting cost to 0 where roads exist.
+#But I can't.
+
+cMap = roads::CLUSexample$roads
+
+rds = raster::rasterToPoints(cMap, fun = function(x){x > 0},
+                             spatial = TRUE)
+rds=spsample(rds,n=1,type="random")
+plot(cMap);plot(rds,add=T)
+
+
+cost <- 1-cMap
+
+nw = roads::projectRoadsNew(landings = cMap,
+                            cost = cost,
+                            roads = rds,roadsOut="raster")
+plot(nw$roads)
